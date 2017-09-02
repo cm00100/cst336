9c45019ed3367a9a55ddc0460b7fdde9f8f05717
Cynthia Martinez <cyn.mart@outlook.com>
2017-09-02 11:43:12 +0000

    adding files and folders

--- index.php
+++ index.php
@@ -2,17 +2,38 @@
 <html>
     <head>
         <link rel="stylesheet" type="text/css" href="css/style.css">
-        <title> </title>
+        <title>Cynthia Martinez</title>
         <meta charset="utf-8"/>
+        
+        <style>
+            li{
+                display: inline-block;
+                
+            }
+            
+        </style>
+        
     </head>
     
     
     <body>
         
-       
-  
+        
+         <p><center><h1><span style='color:white; border-bottom: 1px solid #999;'>Cynthia Martinez</span></h1></center></p>
+         
+         <center>
+         <nav>
+            <ul>
+                <li><a href="index.html" >Home</a></li>
+                <li><a href="index.html" >About</a></li>
+                <li><a href="index.html" >Contact</a></li>
 
-               <p><center><h1><span style='color:white; border-bottom: 1px solid #999;'>Cynthia Martinez</span></h1></center></p>
+            </ul>
+        </nav>
+        </center>
+         
+         
+         <center><img src="img/smiley.png" alt="Smiley face" height="400" width="400"></center>
 
     </body>
 </html>
