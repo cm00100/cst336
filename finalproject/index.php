8cd8a22376ad0aa929af3168ea86fac7244e117a
Cynthia Martinez <cyn.mart@outlook.com>
2017-12-08 13:54:16 +0000

    fixed css link

--- finalproject/index.php
+++ finalproject/index.php
@@ -14,7 +14,7 @@ $conn = getDatabaseConnection();
 <html>
     <head>
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-      <link  href="css/styles.css" rel="stylesheet" type="text/css" />
+      <link  href="css/style.css" rel="stylesheet" type="text/css" />
       <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
 
 
